bf483bb1371a113c258f49b18c70156fca1bac14
Gopalakrishnan-Mohan <gopalakrishnan.mohan@datarynx.com>
2022-05-02 17:52:48 +0530

    changes on Add News

--- src/pages/AddNews/AddNews.jsx
+++ src/pages/AddNews/AddNews.jsx
@@ -2,6 +2,8 @@ import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndro
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./AddNews.css"
+// import laptop from '../../imgaes/laptop_icon.png';
+// import mobile from '../../images/mobile_icon.png';
 
 export default function AddNews() {
   return (
@@ -16,11 +18,11 @@ export default function AddNews() {
         <form className="addnews">
             <input type='checkbox' id="chkbx" tabIndex={0}></input>
             <label className='chkbx-img' id="web-icon">
-                <img src={laptop} alt="laptop" id='laptop_icon'/>
+                {/* <img src={laptop} alt="laptop" id='laptop_icon'/> */}
             </label>
             <input type='checkbox' id="chkbx" tabIndex={0}></input>
             <label className='chkbx-img' id="mobile-icon">
-                <img src={mobile} alt="mobile" id='mobile_icon'/>
+                {/* <img src={mobile} alt="mobile" id='mobile_icon'/> */}
             </label>
             <div className='ad-in'>
                 <span className='titles'>Title</span>
