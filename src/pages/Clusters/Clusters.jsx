752dfac8cee73db865db35c40e43aa6bb023c59a
Truptimayeegithub <Trupti.Pradhan@datarynx.com>
2022-05-03 17:15:43 +0100

    init

--- src/pages/Clusters/Clusters.jsx
+++ src/pages/Clusters/Clusters.jsx
@@ -9,12 +9,92 @@ export default function Clusters() {
     <div className="clusterTitleContainer">
         <h2 >Clusters</h2>
         </div>
-        <div >
-        
-        <div >
-            
+        <div><h1>Hasan part</h1></div>
+        <div>
+        <div  className='container-fluid containerfluidnew'>
+                
+                <div className="col-lg-12 tablediv">
+                    <div>
+                <div style={{paddingTop: "11px"}}>
+                    <span className="searchhead">Search Result</span>
+                    </div>
+                    <div  style={{marginTop: "-25px",textAlign: "right"}}>
+                    <span style={{marginRight: "0.7rem", color: "#86898b"}}><button className="savebtn"><i class="fa fa-check" aria-hidden="true"></i> Save</button></span>
+                    
+
+
+
+<i className="fa fa-expand"></i>
+                </div>
+                </div>
+
+                <div className="col-lg-12 div1" style={{overflowX:"auto"}}>
+
+                    
+                   
+                <table  bordered  >
+                        <thead>
+                            <tr>
+                            <th><input type="checkbox"/></th>
+                            <th>Country ID</th>
+                            <th>Cluster ID</th>
+                            <th>Cluster Name</th>
+                            <th>Target Recruitment Stores</th>
+                            <th>Updated By</th>
+                            <th>Updated On</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+        <tr>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        </tr>
+ <tr>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        </tr>
+                            
+                        </tbody>
+                    </table>
+
+               
+
+                    
+                
+                </div>
+                <div className="tablefooter">
+<span className="fa fa-step-backward linkicon" aria-hidden="true"></span>
+<span className="fa fa-caret-left linkicon" aria-hidden="true"></span>
+<span className="fa fa-caret-right linkicon" aria-hidden="true"></span>
+<span className="fa fa-step-forward linkicon" aria-hidden="true"></span>
+                <div style={{float: "right"}}>0 - 0 of 0 items</div>
+                </div>
+
+                </div>
+                
+            </div>
+            <div><p className="nielsen-footer">Copyright © 2021 Nielsen Consumer LLC. All Rights Reserved.</p></div>
+               
         </div>
-    </div>
 </div>
   )
 }
--- src/pages/ReviewUsers/ReviewUsers.jsx
+++ src/pages/ReviewUsers/ReviewUsers.jsx
@@ -87,13 +87,125 @@ export default function ReviewUsers() {
             &nbsp; &nbsp; &nbsp; &nbsp;
         </div>
    </div>
+   </div>
         <div >
           <div>
-            <h2>secon div</h2>
+          <div  className='container-fluid containerfluid'>
+                
+                <div className="col-lg-12 tablediv">
+                    <div>
+                <div style={{paddingTop: "11px"}}>
+                    <span className="searchhead">Search Result</span>
+                    </div>
+                    <div  style={{marginTop: "-28px",textAlign: "right"}}>
+                    <span style={{marginRight: "0.7rem"}}>Filters</span>
+                    <span><select className="selectoption">
+  <option >Approved</option>
+  <option >Rejected</option>
+  <option >Pending</option>
+  <option>Feedback</option>
+  <option >All</option>
+</select></span>
+<span style={{marginRight: "0.7rem",marginLeft: "0.7rem"}}>Sort</span>
+                    <span><select className="selectoption">
+  <option>Category</option>
+  <option >Date</option>
+</select></span>
+
+
+
+<i className="fa fa-expand"></i>
+                </div>
+                </div>
+
+                <div className="col-lg-12 div1" style={{overflowX:"auto"}}>
+
+                    
+                   
+                <table  bordered  >
+                        <thead>
+                            <tr>
+                            <th>Country(ID)</th>
+                            <th>QCTE ID</th>
+                            <th>QCTE Name</th>
+                            <th>Cluster ID</th>
+                            <th>Cluster Name</th>
+                            <th>Auditor ID</th>
+                            <th>Auditor Name</th>
+                            <th>Idea ID</th>
+                            <th>Idea Name</th>
+                            <th>Category</th>
+                            <th>Date</th>
+                            <th>Status</th> </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+
+        
+
+        
+        </tr>
+        <tr>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        
+        </tr>
+ <tr>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        </tr>
+                            
+                        </tbody>
+                    </table>
+</div>
+<div className="tablefooter">
+<span className="fa fa-step-backward linkicon" aria-hidden="true"></span>
+<span className="fa fa-caret-left linkicon" aria-hidden="true"></span>
+<span className="fa fa-caret-right linkicon" aria-hidden="true"></span>
+<span className="fa fa-step-forward linkicon" aria-hidden="true"></span>
+                <div style={{float: "right"}}>0 - 0 of 0 items</div>
+                </div>
+                </div>
+                </div>
           </div>
             
         </div>
-    </div>
+
+        <div><p className="nielsen-footer">Copyright © 2021 Nielsen Consumer LLC. All Rights Reserved.</p></div>
+            
+    
 </div>
   )
 }
