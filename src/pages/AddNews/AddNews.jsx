91dca8abdb17e322b6401a828b94cb50f6c4a27c
Gopalakrishnan-Mohan <gopalakrishnan.mohan@datarynx.com>
2022-05-04 11:06:24 +0530

    added mobile_icon

--- src/pages/AddNews/AddNews.jsx
+++ src/pages/AddNews/AddNews.jsx
@@ -1,8 +1,8 @@
 
 import "./AddNews.css"
-//import laptop from '/imgaes/laptop_icon.png';
-//import mobile from '../../images/mobile_icon.png';
-import laptop from 'C:/Users/AnishaGanesan/gamification/src/imgaes/laptop_icon.png';
+import laptop from '../../imgaes/laptop_icon.png';
+import mobile from '../../imgaes/mobile_icon.png';
+// import laptop from 'C:/Users/AnishaGanesan/gamification/src/imgaes/laptop_icon.png';
 
 export default function AddNews() {
   return (
@@ -21,7 +21,7 @@ export default function AddNews() {
             </label>
             <input type='checkbox' id="chkbx" tabIndex={0}></input>
             <label className='chkbx-img' id="mobile-icon">
-                {<img src={laptop} alt="mobile" id='mobile_icon'/> }
+                {<img src={mobile} alt="mobile" id='mobile_icon'/> }
             </label>
         </div>
        
